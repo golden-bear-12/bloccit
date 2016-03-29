76535c7236f3d85d9f73a86143e31efba29da5eb
golden-bear-12 <m.honrychs@gmail.com>
2016-03-29 03:24:53 -0400

    assignment-30: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@ require 'random_data'
     body: RandomData.random_paragraph
   )
 end
-
 posts = Post.all
 
 100.times do
@@ -17,5 +16,13 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+puts "find_or_create_by Unique Post finished"
+Post.find_or_create_by(title: "Unique Title", body: "This is our unique body!")
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+puts "find_or_create_by Unique Comment finished"
+Comment.find_or_create_by(body: "This is our unique comment!")
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
