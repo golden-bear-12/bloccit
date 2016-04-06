7d4be87616158918124e33ea10bccd64a87a5d87
golden-bear-12 <m.honrychs@gmail.com>
2016-04-06 15:22:38 -0400

    assignment-32: best attempt

---
+++ spec/models/advertisement_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Advertisement, type: :model do
+  let(:advertisement) { Advertisement.create!(title: "New Advertisement Title", body: "New Advertisement Body", price: "New Advertisement Price") }
+    describe "attributes" do
+      it "responds to title" do
+at        expect(advertisement).to respond_to(:title)
+      end
+      it "responds to body" do
+        expect(advertisement).to respond_to(:body)
+      end
+      it "responds to price" do
+        expect(advertisement).to respond_to(:price)
+      end
+    end
+end
