87b32b5a906aff487fe65cbb945370774468095c
golden-bear-12 <m.honrychs@gmail.com>
2016-04-13 19:46:29 -0400

    assignment-34: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,18 @@ posts = Post.all
   )
 end
 
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_integer
+  )
+end
+sponsored_posts = SponsoredPost.all
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored posts created"
