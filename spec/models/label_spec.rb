b58339d18a67a35e2bfc69047e904e3a12b164ad
golden-bear-12 <m.honrychs@gmail.com>
2016-05-09 23:58:53 -0400

    checkpoint-42: complete

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -22,4 +22,13 @@ RSpec.describe Label, type: :model do
       expect(topic_label).to eql(post_label)
     end
   end
+
+  describe ".update_labels" do
+    it "takes a comma delimited string and returns an array of Labels" do
+      labels = "#{label.name}, #{label2.name}"
+      labels_as_a = [label, label2]
+
+      expect(Label.update_labels(labels)).to eq(labels_as_a)
+    end
+  end
 end
