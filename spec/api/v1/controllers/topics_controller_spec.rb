4ccba60922dd17b2e2ff50ab7d036d3ae7cee3f5
golden-bear-12 <m.honrychs@gmail.com>
2016-05-18 17:14:29 -0400

    checkpoint 49: incomplete

--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -13,6 +13,18 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
       get :show, id: my_topic.id
       expect(response).to have_http_status(:success)
     end
+    it "PUT update returns http unauthenticated" do
+      put :update, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(401)
+    end
+    it "POST create returns http unauthenticated" do
+      post :create, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(401)
+    end
+    it "DELETE destroy returns http unauthenticated" do
+      delete :destroy, id: my_topic.id
+      expect(response).to have_http_status(401)
+    end
   end
 
 
@@ -27,6 +39,18 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
     it "GET show returns http success" do
       get :show, id: my_topic.id
       expect(response).to have_http_status(:success)
+    end
+    it "PUT update returns http forbidden" do
+      put :update, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(403)
+    end
+    it "POST create returns http forbidden" do
+      post :create, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(403)
+    end
+    it "DELETE destroy returns http forbidden" do
+      delete :destroy, id: my_topic.id
+      expect(response).to have_http_status(403)
     end
   end
 end
